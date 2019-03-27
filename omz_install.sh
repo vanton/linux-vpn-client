829478d4b87a7bc14413ea01378b67a141bf2330
vanton <vanton@qq.com>
2019-03-27 14:21:29 +0800

    oh-my-zsh 安装脚本

---
+++ omz_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum -y install zsh zsh-lovers
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# 替换
+sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="tjkirch"/' .zshrc
+sed -i 's/^plugins=(.*)/plugins=(git colored-man-pages colorize command-not-found man cp sudo yum python pip nvm npm node z zsh_reload zsh-syntax-highlighting)/' .zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
